4f160d1e363aeba4baf6545e15c132263244a63d
akyeshi <akyeshi@outlook.com>
2024-05-29 17:29:01 -0400

    finch model added

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    habitat = models.CharField(max_length=100)
+    diet = models.CharField(max_length=100)
+    conservation_status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
