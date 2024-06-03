68e48c3fa06fb600adf78218018af7cc4a457b81
akyeshi <akyeshi@outlook.com>
2024-06-03 10:58:06 -0400

    finchcollector part 4/5 done

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date 
 
 # Create your models here.
 
@@ -17,3 +18,34 @@ class Finch(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
+    
+    def fed_for_today(self): 
+        return self.feeding_set.filter(date=date.today()).count() == len(MEALS)
+
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        'feeding meal',
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    # Create a finch_id FK
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # get_meal_display where is it coming from 
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    # change the default sort
+    class Meta:
+      ordering = ['-date']
+
