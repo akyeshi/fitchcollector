76be8aad6206f46917b2d350ce35cf5c2d1148c4
akyeshi <akyeshi@outlook.com>
2024-06-02 21:48:36 -0400

    finchcollector part 3/5 done

--- main_app/views.py
+++ main_app/views.py
@@ -1,50 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
-''' 
-finches = [
-    {
-        'name': 'Gouldian Finch',
-        'species': 'Chloebia gouldiae',
-        'description': 'Brightly colored with a distinctive rainbow pattern',
-        'habitat': 'Grasslands and savannas of northern Australia',
-        'diet': 'Seeds, grains, and some insects',
-        'conservation_status': 'Near Threatened'
-    },
-    {
-        'name': 'Zebra Finch',
-        'species': 'Taeniopygia guttata',
-        'description': 'Small finch with distinctive black and white stripes',
-        'habitat': 'Arid and semi-arid regions of Australia',
-        'diet': 'Seeds, grains, and some insects',
-        'conservation_status': 'Least Concern'
-    },
-    {
-        'name': 'Canary',
-        'species': 'Serinus canaria',
-        'description': 'Small yellow songbird, popular as a pet',
-        'habitat': 'Originally from the Canary Islands, now domesticated',
-        'diet': 'Seeds, fruits, and some insects',
-        'conservation_status': 'Domesticated'
-    },
-    {
-        'name': 'Hawfinch',
-        'species': 'Coccothraustes coccothraustes',
-        'description': 'Large finch with a massive beak for cracking seeds',
-        'habitat': 'Woodlands and forests across Europe and Asia',
-        'diet': 'Seeds, nuts, and berries',
-        'conservation_status': 'Least Concern'
-    },
-    {
-        'name': "Darwin's Ground Finch",
-        'species': 'Geospiza fortis',
-        'description': 'One of Darwin\'s finches, with a beak adapted for eating seeds',
-        'habitat': 'Galapagos Islands',
-        'diet': 'Seeds, insects, and plant matter',
-        'conservation_status': 'Least Concern'
-    }
-]
-'''
+
 
 # Create your views here.
 
@@ -69,3 +27,18 @@ def finches_detail(request, finch_id):
     return render(request, 'finches/detail.html', {
         'finch': finch
     })
+
+class FinchCreate(CreateView): 
+    model = Finch 
+    # fields = '__all__'
+    fields = ['name', 'species', 'description', 'habitat', 'diet', 'conservation_status']
+    # success_url = '/cats/{id}'  # add a 'success_url' class attribute to the CBV 
+
+class FinchUpdate(UpdateView): 
+    model = Finch 
+    fields = '__all__'
+
+class FinchDelete(DeleteView): 
+    model = Finch 
+    success_url = '/finches'
+    
